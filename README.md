ffd35db147dfe98eb9ce37f3603ff31e04373513
Karthik N <karthik.nagarajan@live.com>
2019-11-05 21:33:12 -0800

    add front-end and server for generics store

---
+++ README.md
@@ -0,0 +1,30 @@
+# The Generics
+
+Band store front that makes bank through the super-cool Stripe API!
+
+## How to Run the app
+
+1. install Node Package Manager (NPM) by installing Node.js
+
+2. from the console move inside the project folder and run
+
+```
+npm install
+```
+
+3. Ensure you have the following settings in an .env file in the project folder (or in .bashrc)
+
+```
+STRIPE_SECRET_KEY=<Stripe secret key>
+STRIPE_PUBLIC_KEY=<Stripe Public key>
+```
+
+4. run the following command to start the app
+
+```
+npm start
+```
+
+5. in the browser type localhost:3000
+
+Enjoy!
---
+++ server.js
@@ -0,0 +1,70 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+// console.log(stripeSecretKey, stripePublicKey)
+
+const express = require('express')
+const app = express()
+const fs = require("fs")
+const stripe = require('stripe')(stripeSecretKey)
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    fs.readFile('items.json', function (error, data) {
+        // console.log(JSON.parse(data)
+        if (error) {
+            res.status(500).end()
+        }
+        else {
+            res.render('index.ejs', {
+                stripePublicKey: stripePublicKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', function (req, res) {
+    fs.readFile('items.json', function (error, data) {
+        if (error) {
+            res.status(500).end()
+        }
+        else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            var total = 0
+            req.body.items.forEach(function (item) {
+                const itemJson = itemsArray.find(function (i) {
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quantity
+            })
+
+            // Create the Stripe charge and send back the amount & Id to user
+            stripe.charges.create(
+                {
+                    amount: total,
+                    source: req.body.stripeTokenId,
+                    currency: 'usd'
+                },
+                function (err, charge) {
+                    // asynchronously called
+                    const totalCharges = (charge.amount / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+                    res.json({ message: `Total charges: ${totalCharges}, ID: ${charge.id}` })
+                }
+            );
+        }
+    })
+})
+
+app.get('/about', function (req, res) {
+    res.render('about.ejs')
+})
+
+app.listen(3000)
