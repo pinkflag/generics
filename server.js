93623337dbf90963d9d694ee326dafa6c6eb539a
Karthik Nagarajan <karthik.nagarajan@live.com>
2022-12-20 10:14:24 -0500

    testing commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000
 const fs = require("fs")
 const stripe = require('stripe')(stripeSecretKey)
 
-app.set('view engine', 'ejs')
+pp.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('public'))
 
